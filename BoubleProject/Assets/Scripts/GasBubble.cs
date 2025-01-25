cef0b27e4c8827c577aff2a730fc61001e77e177
Ole-Marcus-Hansen <olemarhan@gmail.com>
2025-01-25 16:40:50 +0100

    add GasBubble stats

--- BoubleProject/Assets/Scripts/GasBubble.cs
+++ BoubleProject/Assets/Scripts/GasBubble.cs
@@ -1,14 +1,35 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GasBubble : MonoBehaviour
 {
-    public Gas gasType;
+    public float oxygen;
+    public float upgas;
+    public float downgas;
 
-    public enum Gas
+    [SerializeField] Transform bubbleSprite;
+    CircleCollider2D circleCollider;
+
+    void Start()
     {
-        Oxygen,
-        Upgas,
-        Downgas
+        circleCollider = GetComponent<CircleCollider2D>();
+    }
+
+    public void SetGasses(float ox, float up, float down)
+    {
+        oxygen = ox;
+        upgas = up;
+        downgas = down;
+
+        UpdateBubble();
+    }
+
+    protected void UpdateBubble()
+    {
+        bubbleSprite.localScale = Vector3.one * (oxygen + upgas + downgas) / 10f;
+        circleCollider.radius = (oxygen + upgas + downgas) / 10f / 2f;
+
+        //set color
     }
 }
