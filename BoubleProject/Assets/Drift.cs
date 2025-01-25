4e65854b4c57e7362e1e27d0d16302bd464f8d23
notveryhelpfuldude <caracass@hotmail.no>
2025-01-25 11:29:08 +0100

    drift script (store position)

--- BoubleProject/Assets/Drift.cs
+++ BoubleProject/Assets/Drift.cs
@@ -2,17 +2,20 @@ using UnityEngine;
 
 public class Drift : MonoBehaviour
 {
-    private int driftBaseVel = 1;
+    public int driftBaseVel = 1;
+    private Vector3 initialPosition;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        initialPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         
     }
 }
