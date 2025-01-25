c99a48a6e780e868ca740347788efcc779c0a9af
notveryhelpfuldude <caracass@hotmail.no>
2025-01-25 13:14:37 +0100

    Update bubble confinement

--- BoubleProject/Assets/BubbleConfinement.cs
+++ BoubleProject/Assets/BubbleConfinement.cs
@@ -1,7 +1,11 @@
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class BubbleConfinement : MonoBehaviour
 {
+    public Vector2 areaMin = new Vector2(-5f, -5f);
+    public Vector2 areaMax = new Vector2(5f, 5f);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
