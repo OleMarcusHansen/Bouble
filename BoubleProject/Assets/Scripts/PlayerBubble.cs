dddc4c1bdb20068f3e3fbfab5df001d7cdec7afc
Ole-Marcus-Hansen <olemarhan@gmail.com>
2025-01-25 17:05:19 +0100

    add gas tracking in UI

--- BoubleProject/Assets/Scripts/PlayerBubble.cs
+++ BoubleProject/Assets/Scripts/PlayerBubble.cs
@@ -1,13 +1,18 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class PlayerBubble : GasBubble
 {
+    [SerializeField] TMP_Text oxygenText;
+    [SerializeField] TMP_Text upgasText;
+    [SerializeField] TMP_Text downgasText;
 
     // Update is called once per frame
     void Update()
     {
         oxygen -= Time.deltaTime;
+        oxygenText.text = "Oxygen: \n" + Mathf.Round(oxygen * 10f) / 10f;
 
         UpdateBubble();
     }
@@ -17,6 +22,10 @@ public class PlayerBubble : GasBubble
         oxygen += ox;
         upgas += up;
         downgas += down;
+
+        oxygenText.text = "Oxygen: \n" + Mathf.Round(oxygen * 10f) / 10f;
+        upgasText.text = "Upgas: \n" + Mathf.Round(upgas * 10f) / 10f;
+        downgasText.text = "Downgas: \n" + Mathf.Round(downgas * 10f) / 10f;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
